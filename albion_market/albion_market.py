a06d3b99f863772364e38f3660ae6619ebf6e78b
Bogdan Pisica <bpisica@pisica-mini.local>
2025-10-10 11:21:34 -0700

    Initial commit

---
+++ albion_market/albion_market.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+
+class MarketServer:
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    def run(self):
+        #self.app.run(debug=True, host='127.0.0.1', port=5001)
+        self.app.run(debug=True, host='0.0.0.0', port=5001)
+
+server = MarketServer()
+server.run()
